b571d77595cdf82bc5da30ba45d65f5c1324c666
sdc2499 <sdc2499@gmail.com>
2024-03-31 22:10:43 +0300

    זה היה מפחידדד חחחחחחח

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ import express from "express";
 
 import { userRouter } from "./routers/userRouter.js";
 import { postRouter } from "./routers/postRouter.js";
+import { todoRouter } from "./routers/todoRouter.js";
 
 const app = express()
 
 app.use(express.json());
 app.use('/users',userRouter);
 app.use('/post',postRouter);
+app.use('/todo',todoRouter);
 
 app.listen(8081, () => {
     console.log("start server port: 8081");
