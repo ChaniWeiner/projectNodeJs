4da15ef8e455c435e354defc4194a1678eae4d1e
chani weiner <chani03630@gmail.com>
2024-03-31 22:07:50 +0300

    heyy ypu

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 import express from "express";
 
 import { userRouter } from "./routers/userRouter.js";
+import { postRouter } from "./routers/postRouter.js";
 
 const app = express()
 
 app.use(express.json());
 app.use('/users',userRouter);
+app.use('/post',postRouter);
 
 app.listen(8081, () => {
     console.log("start server port: 8081")
---
+++ controllers/postController.js
@@ -0,0 +1,72 @@
+import { Postervice } from '../service/postService.js'
+
+export default class PostController {
+
+    async getPost(req, res, next) {
+        try {
+            const postervice = new Postervice();
+            const data = await postervice.getAllPost();
+            return res.json(data);
+        }
+        catch (err) {
+            return res.status(404).end(`${err}`)
+        }
+
+    }
+
+    async getPostById(req, res, next) {
+        try {
+            const postervice = new Postervice();
+            const data = await postervice.getById(req.params.id);
+            return res.json(data);
+        }
+        catch (err) {
+            return res.status(404).end(`${err}`)
+        }
+ 
+    }
+
+    async addPost(req, res) {
+        try {
+            const Postervice = new Postervice();
+            await Postervice.addPost(req.body);
+            res.status(200).json({ status: 200 });
+        }
+        catch (ex) {
+            return res.status(500).end(`err=${ex}`)
+            // const err = {}
+            // err.status = 500;
+            // err.message = ex;
+            // next(err)
+        }
+    }
+    async deletePost(req, res) {
+        try {
+            const Postervice = new Postervice();
+            await Postervice.deletePost(req.params.id);
+            res.json({ status: 200, data: req.params.id });
+        }
+        catch (ex) {
+            return res.status(500).end(`err=${ex}`)
+            // const err = {}
+            // err.statusCode = 500;
+            // err.message = ex;
+            // next(err)
+        }
+    }
+
+    async updatePost(req, res) {
+        try {
+            const Postervice = new Postervice();
+            await Postervice.updatePost(req.body,req.params.id);
+            res.json({ status: 200, data: req.params.id });
+        }
+        catch (ex) {
+            return res.status(500).end(`${ex}`)
+            // const err = {}
+            // err.statusCode = 500;
+            // err.message = ex;
+            // next(err)
+        }
+    }
+}
